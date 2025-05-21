c3a0e9cede7fd87b1245c6426185e36d5589be3b
cleberjesus2 <cleberjesus493@gmail.com>
2025-05-20 21:57:19 -0300

    feat: Inserindo métodos Put e Delete

--- src/main/java/com/cleber/first_spring_app/controller/HelloWorldController.java
+++ src/main/java/com/cleber/first_spring_app/controller/HelloWorldController.java
@@ -1,6 +1,6 @@
 package com.cleber.first_spring_app.controller;
 
-import com.cleber.first_spring_app.Service.HelloWorldService;
+import com.cleber.first_spring_app.service.HelloWorldService;
 import com.cleber.first_spring_app.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,5 +21,15 @@ public class HelloWorldController {
     @PostMapping("/{id}")
     public String helloWorldPost(@PathVariable String id,@RequestBody User body){
         return "Hello World " + body.getName();
+    }
+
+    @PutMapping("/{id}")
+    public String helloWorldPut(@RequestBody User body){
+        return "Os dados foram alterados" + " " + body.getName();
+    }
+
+    @DeleteMapping
+    public String helloWorldDelete(@RequestBody User body){
+        return "Dados deletados com sucesso !";
     }
 }
